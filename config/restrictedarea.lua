ecb5481f8400d063a6cd140460b24663c866e783
Ekhion76 <76602492+Ekhion76@users.noreply.github.com>
2022-09-19 15:22:41 +0200

    Add files via upload

---
+++ config/restrictedarea.lua
@@ -0,0 +1,20 @@
+Config.restrictedArea = { -- portable workplaces cannot be located within the area, use the builtin command to display it
+
+    -- Paleto Bay Sheriff Office
+    {
+        center = vector3(-454.99, 6009.35, 29.49),
+        radius = 50
+    },
+
+    -- Los Santos PD
+    {
+        center = vector3(459.82, -989.38, 28.69),
+        radius = 45
+    },
+
+    -- Testing Zone in legion square(cube park) REMOVE ME!!! -- use showrestricted command to show!
+    {
+        center = vector3(233.58, -880.76, 30.49),
+        radius = 10
+    }
+}
